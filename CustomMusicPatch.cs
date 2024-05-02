2d4f4584d1615626ee501f19f37bb4efd096c562
dvize <dvize404@gmail.com>
2024-05-02 15:21:23 -0700

    cleaned up old code

--- CustomMusicPatch.cs
+++ CustomMusicPatch.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using Aki.Reflection.Patching;
 using Comfort.Common;
 using DG.Tweening;
-using DG.Tweening.Plugins.Core.PathCore;
 using EFT;
 using EFT.UI;
 using HarmonyLib;
@@ -18,61 +17,48 @@ using UnityEngine.Networking;
 using Random = System.Random;
 
 
-namespace CustomMusic {
-    public class TempCustomMusicComponent : ModulePatch {
-        internal static string CurrDir {
+namespace CustomMusic
+{
+    public class TempCustomMusicComponent : ModulePatch
+    {
+        internal static string CurrDir
+        {
             get; set;
         }
         internal static List<AudioClip> musicClips = new List<AudioClip>();
         internal static string[] files;
-        internal TempCustomMusicComponent() {
+        internal TempCustomMusicComponent()
+        {
             CurrDir = BepInEx.Paths.PluginPath + "/CustomMusic";
             files = Directory.GetFiles(CurrDir + "/music");
         }
-        internal static void LoadAudio(string url) {
-            using (UnityWebRequest web = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG)) {
-                Logger.LogInfo("url: " + url);
-                var operation = web.SendWebRequest();
-                while (!operation.isDone) {
-                }
 
-                if (!web.isNetworkError && !web.isHttpError) {
-                    var clip = DownloadHandlerAudioClip.GetContent(web);
-                    string audioclipname = url.Replace("file:///" + (CurrDir + "/music/").Replace("\\", "/"), "");
-                    clip.name = audioclipname;
-                    Logger.LogInfo("AudioClip: " + clip.name + ", state: " + clip.loadState);
-
-                    musicClips.Add(clip);
-                } else {
-                    var Error = web.error;
-                    Debug.LogErrorFormat("Can't load audio at path:'{0}', error:{1}", new object[]
-                    {
-                        url,
-                        Error
-                    });
-                }
-            }
-        }
-
-        static async Task<AudioClip> LoadClip(string path) {
+        static async Task<AudioClip> LoadClip(string path)
+        {
             AudioClip clip = null;
             Logger.LogInfo("uhmm");
-            using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.MPEG)) {
+            using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.MPEG))
+            {
                 uwr.SendWebRequest();
 
                 // wrap tasks in try/catch, otherwise it'll fail silently
-                try {
-                    while (!uwr.isDone) {
+                try
+                {
+                    while (!uwr.isDone)
+                    {
 
                         Logger.LogInfo("g");
                         await Task.Delay(5);
                     }
 
                     if (uwr.isNetworkError || uwr.isHttpError) Debug.Log($"{uwr.error}");
-                    else {
+                    else
+                    {
                         clip = DownloadHandlerAudioClip.GetContent(uwr);
                     }
-                } catch (Exception err) {
+                }
+                catch (Exception err)
+                {
                     Debug.Log($"{err.Message}, {err.StackTrace}");
                 }
             }
@@ -81,35 +67,32 @@ namespace CustomMusic {
             return clip;
         }
 
-        protected override MethodBase GetTargetMethod() {
+        protected override MethodBase GetTargetMethod()
+        {
             return AccessTools.Method(typeof(GUISounds), "method_1");
         }
 
         [PatchPostfix]
-        private static void PatchPostfix(ref AudioClip[] ___audioClip_0, object __instance) {
-            //clear musicClips
-            //musicClips.Clear();
-
-            //pick 10 or max unique random songs from files and then LoadAudio for each
+        private static void PatchPostfix(ref AudioClip[] ___audioClip_0, object __instance)
+        {
             var random = new System.Random();
-            //CustomMusicPlugin.SongLoadAmount.Value
+
             var randomSongs = files.OrderBy(x => random.Next()).Take(1).ToArray();
-            /*
-                        foreach (var song in randomSongs) {
-                            Logger.LogInfo("random song: " + song);
-                            string url = "file:///" + song.Replace("\\", "/");
-                            LoadAudio(url);
-                        }*/
+
             ConcurrentBag<AudioClip> audioClips = new ConcurrentBag<AudioClip>();
             Parallel.For(0, randomSongs.Length,
-                async i => {
+                async i =>
+                {
                     string url = "file:///" + randomSongs[i].Replace("\\", "/");
                     Logger.LogInfo(url);
                     AudioClip song = await LoadClip(url);
                     Logger.LogInfo($"got song from await! {song.name} asd");
-                    if (song == null) {
+                    if (song == null)
+                    {
                         Logger.LogInfo("Something went wrong, song is null!");
-                    } else {
+                    }
+                    else
+                    {
                         audioClips.Add(song);
                         Logger.LogInfo("adding song!");
                     }
@@ -123,25 +106,33 @@ namespace CustomMusic {
         }
     }
 
-    public class CustomMusicComponent {
-        class Song {
+    public class CustomMusicComponent
+    {
+        class Song
+        {
             public string SongPath;
             public int Frequency;
-            public Song(string songPath, int freq) { this.SongPath = songPath; this.Frequency = freq; }
+            public Song(string songPath, int freq)
+            {
+                this.SongPath = songPath; this.Frequency = freq;
+            }
         }
 
         static Random rand;
         static List<Song> songs = new List<Song>();
 
-        static void InitializeSongShuffler() {
+        static void InitializeSongShuffler()
+        {
             rand = new Random();
             var files = Directory.GetFiles(BepInEx.Paths.PluginPath + "/CustomMusic" + "/music");
-            foreach (var song in files) {
+            foreach (var song in files)
+            {
                 songs.Add(new Song(song, 0));
             }
         }
 
-        static IEnumerator PlaySong(GUISounds instance) {
+        static IEnumerator PlaySong(GUISounds instance)
+        {
             // If nextSong is null, allocate and load the song
             instance.method_4();
             // load and play the song
@@ -155,11 +146,13 @@ namespace CustomMusic {
             yield return null;
 
             AudioClip clip = null;
-            using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(song.SongPath, AudioType.MPEG)) {
+            using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(song.SongPath, AudioType.MPEG))
+            {
                 yield return uwr.SendWebRequest();
 
                 if (uwr.isNetworkError || uwr.isHttpError) Debug.Log($"{uwr.error}");
-                else {
+                else
+                {
                     clip = DownloadHandlerAudioClip.GetContent(uwr);
                 }
             }
@@ -177,14 +170,16 @@ namespace CustomMusic {
 
         [HarmonyPatch(typeof(GUISounds), "method_1")]
         [HarmonyPostfix]
-        static void OnMusicLoad(ref AudioClip[] ___audioClip_0) {
+        static void OnMusicLoad(ref AudioClip[] ___audioClip_0)
+        {
             ___audioClip_0 = null;
             InitializeSongShuffler();
         }
 
         [HarmonyPatch(typeof(GUISounds), "method_3")]
         [HarmonyPrefix]
-        static void ChooseNextSong(GUISounds __instance, ref bool __runOriginal) {
+        static void ChooseNextSong(GUISounds __instance, ref bool __runOriginal)
+        {
             // Skip running the original method
             __runOriginal = false;
             IEnumerator playSong = PlaySong(__instance);
@@ -192,13 +187,16 @@ namespace CustomMusic {
         }
     }
 
-    public class EndScreenLoadPatch : ModulePatch {
-        protected override MethodBase GetTargetMethod() {
+    public class EndScreenLoadPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
             return AccessTools.Method(typeof(TarkovApplication), "method_46");
         }
 
         [PatchPostfix]
-        private static void PatchPostfix() {
+        private static void PatchPostfix()
+        {
 
             //assume replacing original
             var guisounds = Singleton<GUISounds>.Instance;
@@ -210,13 +208,16 @@ namespace CustomMusic {
         }
     }
 
-    public class HideoutScreenLoadPatch : ModulePatch {
-        protected override MethodBase GetTargetMethod() {
+    public class HideoutScreenLoadPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
             return AccessTools.Method(typeof(MainMenuController), nameof(MainMenuController.method_17));
         }
 
         [PatchPostfix]
-        private static void PatchPostfix(MainMenuController __instance) {
+        private static void PatchPostfix(MainMenuController __instance)
+        {
             var guiSounds = Singleton<GUISounds>.Instance;
             guiSounds.method_6(true);
 
--- CustomMusicPlugin.cs
+++ CustomMusicPlugin.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
-using UnityEngine;
 
 namespace CustomMusic
 {
@@ -11,20 +9,10 @@ namespace CustomMusic
     public class CustomMusicPlugin : BaseUnityPlugin
     {
         public static ManualLogSource logger = null;
-        public static ConfigEntry<int> SongLoadAmount
-        {
-            get; set;
-        }
 
         private void Awake()
         {
             logger = Logger;
-            SongLoadAmount = Config.Bind(
-                "Song Loading Configuration", 
-                "Song Load Amount", 
-                5, 
-                new ConfigDescription("Sets the amount of songs to load on Game Start or Raid End. Yes it is randomized. More Songs = more time waiting",
-                null));
 
             //new CustomMusicComponent().Enable();
             new EndScreenLoadPatch().Enable();
